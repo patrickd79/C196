99227154bc2ebb447769f543167834f60b374c8c
Patrick Denney <patrickd79@gmail.com>
2021-02-19 15:36:58 -0800

    can click to all 3 screens and back from home screen

--- app/src/main/java/com/example/patrickdenneymobileapp/AssessmentList.java
+++ app/src/main/java/com/example/patrickdenneymobileapp/AssessmentList.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Objects;
+
 public class AssessmentList extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assessment_list);
+        this.setTitle("Assessments");
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
     }
 }
--- app/src/main/java/com/example/patrickdenneymobileapp/TermList.java
+++ app/src/main/java/com/example/patrickdenneymobileapp/TermList.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Objects;
+
 public class TermList extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_term_list);
+        this.setTitle("Terms");
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
     }
 }
