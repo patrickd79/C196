8cc313208acd2dd33c2b0e125a609768b1b526bf
Patrick Denney <patrickd79@gmail.com>
2021-02-22 13:57:37 -0800

    added standard color for btns, and started the add term activity

--- app/src/main/java/com/example/patrickdenneymobileapp/TermAdd.java
+++ app/src/main/java/com/example/patrickdenneymobileapp/TermAdd.java
@@ -2,24 +2,17 @@ package com.example.patrickdenneymobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import java.util.Objects;
 
-public class TermList extends AppCompatActivity {
+public class TermAdd extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_term_list);
-        this.setTitle("Terms");
+        setContentView(R.layout.activity_term_add);
+        this.setTitle("Add a Term");
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
-    }
-
-    public void openAddTermScreen(View v){
-        Intent intent = new Intent(this, TermAdd.class);
-        startActivity(intent);
     }
 }
